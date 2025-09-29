7e8617974692ce3db1ae7e275a3b97861b584e44
tkdgur2006 <tkdgur2006@gmail.com>
2025-09-29 14:57:17 +0900

    v0.3 types/main.go/

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(math.Ceil(2.31))
-	fmt.Println(strings.Title("go developer~"))
+	/* 같은 의미
+	var name string = "Kim inha"
+	var id int = 1000
+
+	fmt.Println(name, reflect.TypeOf(name))
+	fmt.Println(id, reflect.TypeOf(id))
+	*/
+	var gpa float32 = 3.99 // 원하는 타입으로 만들수 있음 (메모리 절약)
+	name := "Kim inha"
+	id := 1000
+	//gpa := 3.99 double 로 선언됨 (쓸대없이 큰 메모리 차지함)
+
+	fmt.Println(name, reflect.TypeOf(name))
+	fmt.Println(id, reflect.TypeOf(id))
+	fmt.Println(gpa, reflect.TypeOf(gpa))
 }
