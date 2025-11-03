16bd47930fd0b9fa4d37449579a73551c44094d6
“tkdgur2006” <hantaegeon1234@naver.com>
2025-11-03 15:30:38 +0900

    week10 array v0.2

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var arrayBool [3]bool
+	var arrayBool [3]bool = [3]bool{true, false, true}
 	var arrayInt [3]int
 	fmt.Println(arrayBool[1])
-	arrayInt[1]++
-	arrayInt[1] = arrayInt[1] + 1
+	arrayInt[1] = 2
 	fmt.Println(arrayInt[1])
 }
