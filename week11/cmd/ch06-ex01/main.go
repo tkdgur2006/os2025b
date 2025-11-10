36e30b715a70bdc67c61129d79e6b791294f31d2
tkdgur2006 <tkdgur2006@gmail.com>
2025-11-10 14:54:59 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -4,12 +4,13 @@ import "fmt"
 
 func main() {
 	subjects := []string{"Go", "Javascript", "Python", "Linux"}
-	subjectsSlice := subjects[1:3]
+	subjectsSlice := subjects[:3]
+	subjectsSlice[0] = "Java"
 	for _, subjects := range subjects {
 		fmt.Println(subjects)
 	}
 	fmt.Println("===========")
 	for i := 0; i < len(subjectsSlice); i++ {
-		fmt.Println(subjectsSlice)
+		fmt.Println(subjectsSlice[i])
 	}
 }
