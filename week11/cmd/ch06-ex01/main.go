7f0166aba0f0604a93180488a654d6a4e8d87128
tkdgur2006 <tkdgur2006@gmail.com>
2025-11-10 14:28:13 +0900

    week11 slice 0.2v

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -1,12 +1,15 @@
 package main
 
-import ("fmt" )
+import "fmt"
 
-func ()  {
-	var subject []string
-	subject = make([]string, 3)
-	subject[0] = "Go"
-	subject[2] = "Python"
+func main() {
+	/*
+		var subject []string
+		subject = make([]string, 3)
+		subject[0] = "Go"
+		subject[2] = "Python"
+	*/
+	subject := []string{"Go", "", "Python"}
 
 	for _, subject := range subject {
 		fmt.Println(subject)
