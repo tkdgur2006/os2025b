a03ad30cf4e1ebb63f581d0b5e3eddbc0f7e23e9
tkdgur2006 <tkdgur2006@gmail.com>
2025-11-17 15:07:16 +0900

    week12 struct 0.2v

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -14,7 +14,11 @@ func applyPrice(s *subcriber) {
 
 func main() {
 	var s1 subcriber
+	var p *subcriber = &s1
 	//s1.name = "kim sanghyuk"
 	applyPrice(&s1)
 	fmt.Println(s1.name, s1.price)
+	//fmt.Println(*p.price) 이렇게 하면 오류 뜸
+	fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
